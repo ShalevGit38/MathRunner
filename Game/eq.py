24bc2b831ca8342f5bb740beecb418df11be4934
ShalevGit38 <benarishalev3@gmail.com>
2025-05-20 10:21:20 +0300

    Create eq.py

---
+++ Game/eq.py
@@ -0,0 +1,18 @@
+import google.generativeai as genai
+import random
+
+genai.configure(api_key="Aappi")
+
+model = genai.GenerativeModel('gemini-1.5-flash')
+
+equations = []
+
+def getEquations(len):
+    response = model.generate_content(
+        f" {random.randint(1, 100)} generate a list of ({len}) fifth grade math equation, without x, without any = > < signs, (÷ for /, × for multiply, - for minus, + for plus), without space, at a random order with (), when there is multiply show the symbol, between any equation put ?, dont say anything but the equations and ?"
+    )
+    return response.text
+
+def createList(len):
+	global equations
+	equations = getEquations(len).replace("\n", "").split("?")
